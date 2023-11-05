02c27640b7dd9609146e901a1e722dae9bea41eb
nabil931260 <nabil931260@gmail.com>
2023-11-04 19:36:54 -0500

    HomeBuyer class - getter/setter methods, constructor,

--- src/main/java/homebuy/LoadEval.java
+++ src/main/java/homebuy/LoadEval.java
@@ -2,5 +2,10 @@ package homebuy;
 import java.util.*;
 
 public class LoadEval {
+    // Constants
+    private final int MIN_CREDIT_SCORE = 640;
+    private final double MAX_LTV = 0.95; 
+    private final double MAX_DTI = 0.43; 
+    private final double MAX_FEDTI = 0.28; 
 
 }
